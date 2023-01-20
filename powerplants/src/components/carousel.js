596834bbde2ff79655732b20f2f1fd9ce910d3e6
Max Tobias Weber <tobias.weber@wheregroup.com>
2023-01-20 10:43:36 +0100

    refactor WmsCarousel to use flexbox

--- powerplants/src/components/carousel.js
+++ powerplants/src/components/carousel.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, AppBar, Grid, useMediaQuery } from "@mui/material/";
+import { Button, Box, Grid, useMediaQuery } from "@mui/material/";
 import IconButton from "@mui/material/IconButton";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -9,9 +9,9 @@ export default function WmsCarousel(props) {
   const mediaIsLandscape = useMediaQuery("(max-height: 562px)");
 
   useEffect(() => {
-    if(props.current !== showIndex){
+    if (props.current !== showIndex) {
       props.setter(showIndex);
-    }    
+    }
   }, [showIndex]);
 
   function clickLeft() {
@@ -31,38 +31,39 @@ export default function WmsCarousel(props) {
   }
 
   return (
-     <>
-        <AppBar
-           position="fixed"
-           sx={{
-              top: "auto",
-              bottom: mediaIsLandscape ? 4 : 38,
-              left: mediaIsLandscape ? 120 : 4,
-              width: 190,
-              height: 115,
-              opacity: 0.8,
-           }}
+    <>
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: mediaIsLandscape ? 4 : 38,
+          left: mediaIsLandscape ? 120 : 4,
+          height: 65,
+          opacity: 0.8,
+          zIndex: 110,
+          display: "flex",
+          justifyContent: "space-evenly",
+          backgroundColor: "#353535",
+        }}
+      >
+        <IconButton onClick={clickLeft}>
+          <ArrowBackIosIcon color="secondary" />
+        </IconButton>
+        <Button
+          onClick={() => props.setter(showIndex)}
+          sx={{ display: "flex", alignItems: "stretch" }}
         >
-           <Grid>
-              <IconButton onClick={clickLeft}>
-                 <ArrowBackIosIcon color="secondary" />
-              </IconButton>
-              <Button onClick={() => props.setter(showIndex)}>
-                 <img
-                    src={props.options[showIndex].image}
-                    alt={props.options[showIndex].title}
-                    style={{
-                       width: 100,
-                       height: 100,
-                       opacity: 1,
-                    }}
-                 />
-              </Button>
-              <IconButton onClick={clickRight} sx={{ bottom: 76, left: 156 }}>
-                 <ArrowForwardIosIcon color="secondary" />
-              </IconButton>
-           </Grid>
-        </AppBar>
-     </>
+          <img
+            src={props.options[showIndex].image}
+            alt={props.options[showIndex].title}
+            style={{
+              opacity: 1,
+            }}
+          />
+        </Button>
+        <IconButton onClick={clickRight}>
+          <ArrowForwardIosIcon color="secondary" />
+        </IconButton>
+      </Box>
+    </>
   );
 }
