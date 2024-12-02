942d6ff371599832ba2aaa9671a9e448fad726cf
JannikBrack <88673060+JannikBrack@users.noreply.github.com>
2024-12-02 15:14:15 +0000

    Deploying to main from  @ 63d672a177224c8ab8f080fa1775f92bd757fc85 🚀

--- docs/multi_tab/service-worker.js
+++ docs/multi_tab/service-worker.js
@@ -5,12 +5,15 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   console.log(`Service Worker activating. ${event}`);
+  event.waitUntil(self.clients.claim());  // Macht den SW sofort aktiv
 });
+
 
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'BROADCAST') {
     self.clients.matchAll({ includeUncontrolled: true, type: 'window' }).then(clients => {
       clients.forEach(client => {
+        console.log(client);
         client.postMessage(event.data.message);
       });
     });
